4fa43a6585d812e70d3a0f1784cd256d14f02560
Mike Lee Williams <mike@mike.place>
2017-07-29 15:38:27 -0700

    Initial commit

---
+++ dwx.py
@@ -0,0 +1,77 @@
+import bisect
+import datetime
+import os
+import pytz
+import requests
+
+ds_key = os.environ['DS_KEY']
+ds_url = 'https://api.darksky.net/forecast'
+ds_apiparams = {'exclude': 'hourly,alerts,minutely', 'units': 'auto'}
+
+
+def dwx_tz():
+    return pytz.timezone(os.environ['DWX_TZ'])
+
+
+def find_adjective(adjectives, cuts, number):
+    return adjectives[bisect.bisect(cuts, number)]
+
+
+def describe_temperature_change(today, yesterday, ystring='yesterday'):
+    temp_adjectives = ['much cooler than', 'a little cooler than',
+                       'about the same as', 'a little warmer than',
+                       'much warmer than']
+    if today['units'] == 'us':
+        temp_cuts = [-10.0, -5.0, 5.0, 10.0]
+    else:
+        temp_cuts = [-5.5, -2.5, 2.5, 5.5]
+    deltat = today['temperatureMax'] - yesterday['temperatureMax']
+    return f"{find_adjective(temp_adjectives, temp_cuts, deltat)} {ystring}"
+
+
+def describe_temperature(today, yesterday):
+    return (f"High of {round(today['temperatureMax'])} " +
+            f"({describe_temperature_change(today, yesterday)})")
+
+
+def get_wx(date=None, offset=0):
+    if not date:
+        date = datetime.datetime.now()
+    if offset:
+        date += datetime.timedelta(days=offset)
+    url_args = [os.environ['DWX_LATITUDE'],
+                os.environ['DWX_LONGITUDE'],
+                str(round(date.timestamp()))]
+    url = f"{ds_url}/{ds_key}/{','.join(url_args)}"
+    wx = requests.get(url, params=ds_apiparams).json()
+    return dict(
+        wx['daily']['data'][0],
+        **{'units': wx['flags']['units']}
+    )
+
+
+def make_sentence(st):
+    """Capitalize and add a full stop to a string."""
+    st = st.strip()
+    st = st[0].upper() + st[1:]
+    return st + ('' if st.endswith('.') else '.')
+
+
+def local_dow_from_ts(ts, tz):
+    """Return string day of the week given epoch timestamp and timezone."""
+    return (datetime.datetime.fromtimestamp(ts)
+            .astimezone(tz)
+            .strftime('%A'))
+
+
+def describe_wx():
+    wx_yesterday = get_wx(offset=-1)
+    wx_today = get_wx()
+    facts = [local_dow_from_ts(wx_today['time'], dwx_tz()),
+             wx_today['summary'],
+             describe_temperature(wx_today, wx_yesterday)]
+    return ' '.join(map(make_sentence, facts))
+
+
+if __name__ == '__main__':
+    print(describe_wx())
---
+++ tweet.py
@@ -0,0 +1,59 @@
+import datetime
+import os
+import tweepy
+import dwx
+
+post_time = datetime.time(7, 30)
+post_time_tol_seconds = 5 * 60
+
+
+def post_time_today(dt):
+    """Return today's post_time as a fully qualified datetime."""
+    return dt.replace(hour=post_time.hour,
+                      minute=post_time.minute,
+                      second=0, microsecond=0)
+
+
+def times():
+    """Return time in server timezone, local time in location timezone, and
+    target post_time in location timezone."""
+    server = datetime.datetime.now().astimezone()
+    local = server.astimezone(dwx.dwx_tz())
+    target = post_time_today(local)
+    return server, local, target
+
+
+def check_time():
+    """Return True if within post_time_tol_seconds of post_time in timezone of
+    location."""
+    sever, local, target = times()
+    time_difference = abs((local - target).total_seconds())
+    return time_difference < post_time_tol_seconds
+
+
+def make_api():
+    auth = tweepy.OAuthHandler(os.environ['TW_CONSUMERKEY'],
+                               os.environ['TW_CONSUMERKEYSECRET'])
+    auth.set_access_token(os.environ['TW_ACCESSTOKEN'],
+                          os.environ['TW_ACCESSTOKENSECRET'])
+    return tweepy.API(auth)
+
+
+def post(tweet):
+    api = make_api()
+    api.update_status(tweet)
+
+
+def format_times(server, local, target):
+    return '\n'.join([f"Server: {server}",
+                      f"Local: {local}",
+                      f"Target: {target}"])
+
+
+def check_time_and_post():
+    print(format_times(*times()))
+    if check_time():
+        tweet = dwx.describe_wx()
+        post(tweet)
+    else:
+        print('Not time yet.')
